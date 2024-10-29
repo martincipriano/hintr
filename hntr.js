96f2410d8822472aca75d0be25d4a0ee05ffe5d9
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-29 23:57:23 +0800

    Add a method for event listeners and init

---
+++ hntr.js
@@ -0,0 +1,13 @@
+window.hntr = {}
+
+window.hntr.init = function() {
+}
+
+window.hntr.eventListeners = function() {
+  
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.hntr.init()
+  window.hntr.eventListeners()
+})
