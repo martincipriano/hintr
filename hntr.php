801f3920444609fba6ed93e2f913489dfc826a20
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-29 23:52:00 +0800

    Enqueue the plguin stylesheet

--- hntr.php
+++ hntr.php
@@ -7,3 +7,10 @@
  * Version: 0.0.1
  */
 
+
+if (!function_exists('hntr_enqueue_scripts')) {
+  add_action('wp_enqueue_scripts', 'hntr_enqueue_scripts');
+  function hntr_enqueue_scripts() {
+    wp_enqueue_style('hntr', plugin_dir_url(__FILE__) . 'hntr.css', [], filemtime(plugin_dir_path(__FILE__) . 'hntr.css'));
+  }
+}
