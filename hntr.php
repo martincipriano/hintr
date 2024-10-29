6f4a637910db6e1efb2a58ec2a4f5d5e5742a7fd
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-29 23:53:08 +0800

    Enqueue the javascript

--- hntr.php
+++ hntr.php
@@ -12,5 +12,6 @@ if (!function_exists('hntr_enqueue_scripts')) {
   add_action('wp_enqueue_scripts', 'hntr_enqueue_scripts');
   function hntr_enqueue_scripts() {
     wp_enqueue_style('hntr', plugin_dir_url(__FILE__) . 'hntr.css', [], filemtime(plugin_dir_path(__FILE__) . 'hntr.css'));
+    wp_enqueue_script('hntr', plugin_dir_url(__FILE__) . 'hntr.js', ['jquery'], filemtime(plugin_dir_path(__FILE__) . 'hntr.js'), true);
   }
 }
