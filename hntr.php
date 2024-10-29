3b092b02966c22d79c073e4aef1188188aaa6021
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-29 23:49:32 +0800

    Rename the plugin

--- hntr.php
+++ hntr.php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * Plugin Name: Hinter
+ * Plugin Name: Hntr
  * Description: A plugin that adds search suggestions to the search form.
  * Author: Martin Cipriano
  * Version: 0.0.1
  */
+
