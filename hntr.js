bd245219818adc703aa6de8590c2f21161a321c8
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-30 00:29:15 +0800

    Create an event listener to each input element and listen to the keyup event. When the user types in the input, check if there are 3 or more characters in the input. If there are, show the list element.

--- hntr.js
+++ hntr.js
@@ -24,6 +24,20 @@ window.hntr.eventListeners = function() {
   // Get all inputs with data-hntr="true"
   let inputs = document.querySelectorAll('[type="text"][data-hntr="true"]')
 
+  // Add an event listener to each input element
+  inputs.forEach(input => {
+
+    // Listen to the keyup event
+    input.addEventListener('keyup', function() {
+
+      // Get the hntr list element
+      let list = input.nextElementSibling
+
+      // Check if there are 3 or more characters in the input
+      if (input.value.length >= 3 && list.classList.contains('show') === false) {
+      }
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
