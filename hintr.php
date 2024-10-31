b90801a177eed2cc2e8bc59808b1dedde76ece0f
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-31 19:50:19 +0800

    Create a function that will check if theres an upload directory. If there isnt, create it or show an error

--- hintr.php
+++ hintr.php
@@ -7,6 +7,26 @@
  * Version: 0.0.1
  */
 
+/**
+ * Activate the plugin.
+ * 
+ * This function is called when the plugin is activated.
+ * It checks if the uploads directory exists and is writable.
+ * If it does not exist or is not writable, it displays an error message.
+ * 
+ * @return void
+ * 
+ * @since 0.0.1
+ */
+if (!function_exists('hintr_activate')) {
+  register_activation_hook(__FILE__, 'hintr_activate');
+  function hintr_activate() {
+    if (!wp_mkdir_p(ABSPATH . 'wp-content/uploads/hintr') || !is_writable(ABSPATH . 'wp-content/uploads/hintr')) {
+      wp_die('The plugin needs an upload directory at wp-content/uploads/hintr that is writable.');
+    }
+  }
+}
+
 if (!function_exists('hintr_enqueue_scripts')) {
   add_action('wp_enqueue_scripts', 'hintr_enqueue_scripts');
   function hintr_enqueue_scripts() {
