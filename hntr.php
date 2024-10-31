8b64dc72ec585e82cbd5af477020902eef4f492a
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-10-31 19:42:08 +0800

    Rename classes and function from hntr to hintr

--- hntr.php
+++ hntr.php
@@ -7,10 +7,10 @@
  * Version: 0.0.1
  */
 
-if (!function_exists('hntr_enqueue_scripts')) {
-  add_action('wp_enqueue_scripts', 'hntr_enqueue_scripts');
-  function hntr_enqueue_scripts() {
-    wp_enqueue_style('hntr', plugin_dir_url(__FILE__) . 'hntr.css', [], filemtime(plugin_dir_path(__FILE__) . 'hntr.css'));
-    wp_enqueue_script('hntr', plugin_dir_url(__FILE__) . 'hntr.js', ['jquery'], filemtime(plugin_dir_path(__FILE__) . 'hntr.js'), true);
+if (!function_exists('hintr_enqueue_scripts')) {
+  add_action('wp_enqueue_scripts', 'hintr_enqueue_scripts');
+  function hintr_enqueue_scripts() {
+    wp_enqueue_style('hintr', plugin_dir_url(__FILE__) . 'hintr.css', [], filemtime(plugin_dir_path(__FILE__) . 'hintr.css'));
+    wp_enqueue_script('hintr', plugin_dir_url(__FILE__) . 'hintr.js', ['jquery'], filemtime(plugin_dir_path(__FILE__) . 'hintr.js'), true);
   }
 }
