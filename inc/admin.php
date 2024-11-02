7d1a34c29104ef16aa559c73e841e58c57c4bc89
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-11-02 13:25:53 +0800

    Rename the functions and add the template for the options

--- inc/admin.php
+++ inc/admin.php
@@ -2,9 +2,25 @@
 
 // Create an options page for the plugin.
 
-if (!function_exists('hntr_create_options_page')) {
-  add_action('admin_menu', 'hntr_create_options_page');
-  function hntr_create_options_page() {
-    add_options_page('Hntr Options', 'Hntr', 'manage_options', 'hntr', 'hntr_options_page');
+if (!function_exists('hntr_create_options')) {
+  add_action('admin_menu', 'hntr_create_options');
+  function hntr_create_options() {
+    add_options_page('Hntr Options', 'Hntr', 'manage_options', 'hntr', 'hntr_options');
+  }
+}
+
+if (!function_exists('hntr_options')) {
+  function hntr_options() { ?>
+    <div class="wrap">
+      <h2>Hntr Options</h2>
+      <form action="options.php" method="post">
+        <?php
+          settings_fields('hntr_options');
+          do_settings_sections('hntr');
+          submit_button();
+        ?>
+      </form>
+    </div>
+    <?php
   }
 }
