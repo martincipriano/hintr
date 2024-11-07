826dcaa0290f5893ae6f6fbddc9b9f8b58e2eeda
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2024-11-07 19:08:57 +0800

    Update the css and js path because it was moved to the assets directory

--- inc/public.php
+++ inc/public.php
@@ -127,8 +127,8 @@ if (!function_exists('hintr_enqueue_scripts')) {
     $plugin_path = plugin_dir_path(dirname(__FILE__));
     $plugin_url = plugin_dir_url(dirname(__FILE__));
 
-    wp_enqueue_style('hintr', $plugin_url . 'hintr.css', [], filemtime($plugin_path . 'hintr.css'));
-    wp_enqueue_script('hintr', $plugin_url . 'hintr.js', ['jquery'], filemtime($plugin_path . 'hintr.js'), true);
+    wp_enqueue_style('hintr', $plugin_url . 'hintr.css', [], filemtime($plugin_path . 'assets/css/hintr.css'));
+    wp_enqueue_script('hintr', $plugin_url . 'hintr.js', ['jquery'], filemtime($plugin_path . 'assets/js/hintr.js'), true);
     wp_localize_script('hintr', 'hintrData', [
       'uploadDir' => wp_upload_dir()['baseurl'] . '/hintr/',
       'hint' => '<li><a class="hintr-nav-item" href="url">title</a></li>'
