7c2cb3e4b0e4676d4bc6806631351cf114198ff9
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-18 22:56:25 +0800

    Show an admin notice, an error if the uploads directory is not writable or if it doesn't exist.
    - Create a property for the uploads path.
    - Create a method for the admin notice and hook it into the admin_notices action.

--- includes/admin.php
+++ includes/admin.php
@@ -1,12 +1,14 @@
 <?php
 
 class Hintr_Admin {
+  private $uploads_path;
   private $plugin_path;
   private $plugin_url;
   private $plugin_settings;
   private $initial_plugin_settings;
 
   public function __construct() {
+    $this->uploads_path = ABSPATH . 'wp-content/uploads';
     $this->plugin_path = plugin_dir_path(dirname(__FILE__));
     $this->plugin_url = plugin_dir_url(dirname(__FILE__));
     $this->plugin_settings = get_option('hintr_settings');
@@ -18,6 +20,7 @@ class Hintr_Admin {
     ];
 
     register_activation_hook($this->plugin_path . 'hintr.php', [$this, 'activate']);
+    add_action('admin_notices', [$this, 'display_error']);
   }
 
   public function activate() : void
@@ -36,5 +39,14 @@ class Hintr_Admin {
 
     // Create the plugin settings in the wp_options table
     update_option('hintr_settings', $initial_settings);
+  }
+
+  public function display_error() : void
+  {
+    // Check if the uploads directory exists and is writable
+    // If it doesn't, display an error message
+    if (!is_writable($this->uploads_path)) {
+      echo '<div class="error"><p>Please ensure that the "uploads" directory exists in the "wp-content" folder and has the necessary write permissions.</p></div>';
+    }
   }
 }
