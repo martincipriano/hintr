7efe89013c8b0813b1b9282af223f4f786c0bd5c
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-18 22:14:45 +0800

    Create a property for the plugin's upload url and add it to the localized script for front-end use

--- hintr.php
+++ hintr.php
@@ -12,11 +12,13 @@
 class Hintr {
   private $plugin_path;
   private $plugin_url;
+  private $plugin_uploads_url;
   private $plugin_settings;
 
   public function __construct() {
     $this->plugin_path = plugin_dir_path(__FILE__);
     $this->plugin_url = plugin_dir_url(__FILE__);
+    $this->plugin_uploads_url = wp_upload_dir()['baseurl'] . '/hintr/';
     $this->plugin_settings = get_option('hintr_settings');
   }
 
@@ -26,6 +28,7 @@ class Hintr {
     wp_enqueue_script('hintr', $this->plugin_url . 'assets/js/hintr.js', [], filemtime($this->plugin_path . 'assets/js/hintr.js'), true);
 
     wp_localize_script('hintr', 'hintr', [
+      'uploads_url' => $this->plugin_uploads_url,
       'search_in' => $this->plugin_settings
     ]);
   }
