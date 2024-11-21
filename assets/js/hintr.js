c1915accc19c3fbc8078ab03e13b4c5923c96118
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-22 05:15:55 +0800

    Query all the input text or search with the data-hintr attribute or name attribute with "s" value

--- assets/js/hintr.js
+++ assets/js/hintr.js
@@ -90,6 +90,7 @@ window.hintr.hideSuggestions = function(e) {
 }
 
 window.hintr.eventListeners = function() {
+  let inputs = document.querySelectorAll('[type="text"][data-hintr], [type="search"][data-hintr], [name=s]')
 }
 
 document.addEventListener('DOMContentLoaded', function() {
