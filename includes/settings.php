7a1399dfc2de9eb256869ad167e4b3c40db6f045
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-21 21:55:41 +0800

    Create a method or adding an options page and hook into admin_menu.

--- includes/settings.php
+++ includes/settings.php
@@ -3,6 +3,18 @@
 class Hintr_Settings {
   public function __construct()
   {
+    add_action('admin_menu', [$this, 'create_settings_page']);
+  }
+
+  public function create_settings_page() : void
+  {
+    $page_title = 'Hintr Options';
+    $menu_title = 'Hintr';
+    $capability = 'manage_options';
+    $slug       = 'hintr';
+    $callback   = [$this, 'settings_page'];
+
+    add_options_page($page_title, $menu_title, $capability, $slug, $callback);
   }
 }
 
