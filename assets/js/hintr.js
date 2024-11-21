adff48e19c7b139fee7bc464239d0cdee0af0c44
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-22 05:04:34 +0800

    Search for text and search inputs that has a data-hintr attribute or a name attrobute with "s" as a value. Create and insert an element next to it.

--- assets/js/hintr.js
+++ assets/js/hintr.js
@@ -1,6 +1,20 @@
 window.hintr = {}
 
 window.hintr.init = function() {
+
+  let inputs = document.querySelectorAll('[type="text"][data-hintr], [type="search"][data-hintr], [name=s]')
+
+  inputs.forEach(input => {
+    let element = document.createElement('ul')
+    let rect = input.getBoundingClientRect()
+
+    element.classList.add('hintr')
+    element.style.top = rect.top + input.offsetHeight + 'px'
+    element.style.left = rect.left + 'px'
+    element.style.width = input.offsetWidth + 'px'
+
+    input.parentNode.insertBefore(element, input.nextSibling)
+  })
 }
 
 window.hintr.eventListeners = function() {
