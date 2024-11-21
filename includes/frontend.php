d80f8d2f846589efe53ff140a421a311bcefa422
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-22 05:22:04 +0800

    Fix the plugin path property

--- includes/frontend.php
+++ includes/frontend.php
@@ -1,13 +1,15 @@
 <?php
 
 class Hintr {
-  private $plugin_url;
-  private $plugin_uploads_url;
-  private $plugin_settings;
+  protected $plugin_url;
+  protected $plugin_path;
+  protected $plugin_uploads_url;
+  protected $plugin_settings;
 
   public function __construct()
   {
     $this->plugin_url = plugin_dir_url(dirname(__FILE__));
+    $this->plugin_path = plugin_dir_path(dirname(__FILE__));
     $this->plugin_uploads_url = wp_upload_dir()['baseurl'] . '/hintr/';
     $this->plugin_settings = get_option('hintr_settings');
 
@@ -19,7 +21,7 @@ class Hintr {
     wp_enqueue_style('hintr', $this->plugin_url . 'assets/css/hintr.css', [], filemtime($this->plugin_path . 'assets/css/hintr.css'), 'all');
     wp_enqueue_script('hintr', $this->plugin_url . 'assets/js/hintr.js', [], filemtime($this->plugin_path . 'assets/js/hintr.js'), true);
 
-    wp_localize_script('hintr', 'hintr', [
+    wp_localize_script('hintr', 'hintrSettings', [
       'hint' => '<li><a class="hint" href="url">title</a></li>',
       'search_in' => $this->plugin_settings,
       'uploads_url' => $this->plugin_uploads_url
