1312f6484bb5d5c65c59af64c5fedb0f78ed551c
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-21 21:58:44 +0800

    Add a method containing the settings page HTML

--- includes/settings.php
+++ includes/settings.php
@@ -16,6 +16,22 @@ class Hintr_Settings {
 
     add_options_page($page_title, $menu_title, $capability, $slug, $callback);
   }
+
+  public function settings_page() : void
+  {
+    ?>
+    <div class="wrap">
+      <h1><?= _e('Hintr Options', 'hintr'); ?></h1>
+      <form method="post" action="options.php">
+        <?php
+          settings_fields('hintr_settings');
+          do_settings_sections('hintr');
+          submit_button('Save Settings');
+        ?>
+      </form>
+    </div>
+    <?php
+  }
 }
 
 new Hintr_Settings;
