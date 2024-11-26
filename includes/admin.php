6d523d8fc29b0a24957af4ac06997881ab5dda26
Jose Martin Cipriano <martincipriano@gmail.com>
2024-11-26 23:30:07 +0800

    Perform security related updates
    - Add a fallback if filemtime returns an error in the enqueue_script method
    - Update the method's access modifiers from public to protected if it's not gonna be accessed from outside the class

--- includes/admin.php
+++ includes/admin.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!defined('ABSPATH')) {
+  exit;
+}
+
 class Hintr_Admin {
   protected $plugin_url;
   protected $plugin_path;
@@ -25,20 +29,25 @@ class Hintr_Admin {
   public function enqueue_scripts() : void
   {
     wp_enqueue_style('slim-select', $this->plugin_url . 'assets/css/slimselect.min.css', [], '2.9.2');
-    wp_enqueue_style('hintr-admin', $this->plugin_url . 'assets/css/hintr-admin.css', [], filemtime($this->plugin_path . 'assets/css/hintr-admin.css'));
+    wp_enqueue_style('hintr-admin', $this->plugin_url . 'assets/css/hintr-admin.css', [], (filemtime($this->plugin_path . 'assets/css/hintr-admin.css') ? filemtime($this->plugin_path . 'assets/css/hintr-admin.css') : '1.0.0'));
 
     wp_enqueue_script('slim-select', $this->plugin_url . 'assets/js/slimselect.min.js', [], '2.9.2', true);
-    wp_enqueue_script('hintr-admin', $this->plugin_url . 'assets/js/hintr-admin.js', ['slim-select'], filemtime($this->plugin_path . 'assets/js/hintr-admin.js'), true);
+    wp_enqueue_script('hintr-admin', $this->plugin_url . 'assets/js/hintr-admin.js', ['slim-select'], (filemtime($this->plugin_path . 'assets/js/hintr-admin.js') ? filemtime($this->plugin_path . 'assets/js/hintr-admin.js') : '1.0.0'), true);
   }
 
-  public function activate() : void
+  protected function activate() : void
   {
     update_option('hintr_settings', $this->initial_plugin_settings);
   }
 
-  public function get_meta_keys($post_type) : array
+  protected function get_meta_keys($post_type = []) : array
   {
     global $wpdb;
+
+    if (!post_type_exists($post_type)) {
+      return [];
+    }
+
     $query = $wpdb->prepare("SELECT DISTINCT pm.meta_key FROM {$wpdb->postmeta} pm INNER JOIN {$wpdb->posts} p ON pm.post_id = p.ID WHERE p.post_type = %s", $post_type);
     return $wpdb->get_col($query);
   }
